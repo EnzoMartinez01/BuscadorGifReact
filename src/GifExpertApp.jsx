fdd58cfa180217538e8b45f3fe7a6e46f71aabb9
EnzoMartinez01 <enzomartinez2412@gmail.com>
2023-07-27 16:04:51 -0500

    secondcommit

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -13,16 +13,33 @@ export default function GifExpertApp() {
         setCategories([newCategory, ...categories]);
         //  setCateories(cat => [...cat, 'Valorant']);
     }
+
+    const eliminarCategory = (busquedaEliminar) => {
+        setCategories((prevBusquedas) =>
+            prevBusquedas.filter((categories) => categories !== busquedaEliminar));
+    };
+
+    const elimnarBusqueda = () => {
+        setCategories("");
+    }
+
   return (
     <>
         {/*titutlo*/}
         <h1>GifExpertApp</h1>
 
         {/*Input*/}
+        
         <AddCategory 
             onNewCategory={(event) => onAddCategory(event)}
             currentCategories={categories}
         />
+        {categories.map((categories, index) => (
+            <div key={index}>
+                {categories}
+                <button onClick={() => eliminarCategory(categories)}>Eliminar Busqueda</button>
+            </div>
+        ))}
 
         {/*Listado de Gif */}
         {
